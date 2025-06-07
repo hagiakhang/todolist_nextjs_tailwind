f28441f36a6ead8dfa4290c72958da39fd99cc64
tuitenthai123 <21004148@st.vlute.edu.vn>
2025-06-07 09:55:22 +0700

    fix lại config

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <div className='text-5xl text-sky-500'>
+        fix lại config nè
+      </div>
+    </div>
+  )
+}
+
+export default page
